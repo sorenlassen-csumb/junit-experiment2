de76ae0a97c34d3736dc6a928067cf6794c9ab9d
Soren Lassen <soren@fb.com>
2016-08-25 03:25:13 -0700

    Fresh Java maven project
    Summary: mvn archetype:generate -DgroupId=edu.csumb.cst438fall2016.slassen -DartifactId=junit-experiment -DinteractiveMode=false
    
    Test Plan: mvn test
    
    Reviewers: sorenlassen-csumb-test
    
    Reviewed By: sorenlassen-csumb-test
    
    Differential Revision: https://reviews.facebook.net/D62547

---
+++ src/main/java/edu/csumb/cst438fall2016/slassen/App.java
@@ -0,0 +1,13 @@
+package edu.csumb.cst438fall2016.slassen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
